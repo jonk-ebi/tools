958c246d08711d141647ebb1c95f0528e47f7b06
Jon Keatley <jon@ebi.ac.uk>
2023-04-21 14:40:18 +0100

    cleaned up personalissues module

--- personalissues/run-backend.sh
+++
@@ -1,2 +0,0 @@
-poetry run python -m personalissues -t NjEwMzYzMjkzOTM3OskfDxR/62LD2lEQ07u2tGJ3ishA -p kamal andres tuanle mcharkhchi -o backend.csv -l ENSWEB
-
--- personalissues/run.sh
+++
@@ -1,1 +0,0 @@
-poetry run python -m personalissues -t NjEwMzYzMjkzOTM3OskfDxR/62LD2lEQ07u2tGJ3ishA -p imran andrey jyo -o frontend.json -l ENSWEB --summary true
