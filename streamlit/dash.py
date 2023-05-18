f6fb50fe43993dea15bf29ce245036c2fd66be1f
Jon Keatley <jon@ebi.ac.uk>
2023-05-18 17:00:03 +0100

    Moved data to a class so enable delta features

--- streamlit/dash.py
+++ streamlit/dash.py
@@ -3,7 +3,7 @@ import toml
 import streamlit as st
 import pandas as pd
 
-from data import build_personal_load, build_issues, update_data, build_assigned_tickets, build_all_assigned_tickets
+from data import JiraData
 
 # TODO
 # - factor in project people 
@@ -13,6 +13,7 @@ from data import build_personal_load, build_issues, update_data, build_assigned_
 # - add a my issues tab
 # - days since assigned 
 # - delta on summary
+# - project specfic 
 
 def load_settings(toml_path):
     with open(toml_path) as toml_stream:
@@ -21,15 +22,38 @@ def load_settings(toml_path):
 
 def update_all_projects(projct_list, team_list, token): 
     for project in projct_list: 
-        update_data(project, team_list, token)
+        d.update_data(project, team_list, token)
         
 config = load_settings("settings.toml")
 project_list = config['settings']['projects']
+d = JiraData(project_list)
+
 team_list = config['settings']['team']
 project_settings = {project:config[project] for project in project_list}
+
 project_name_list = [f"{p} ({project_settings[p]['name']})" for p in project_list]
 
+#-- Setup dashboard 
 st.set_page_config(page_title="Ensembl Jira Dash", page_icon=":chart_with_upwards_trend:", layout="wide")
+
+st.markdown("""
+<style>
+     div[data-testid="stMetricValue"]
+     {
+        display:inline-block; 
+        width:70%;
+     }
+     
+     div[data-testid="stMetricDelta"]
+     {
+         display:inline-block; 
+         width:30%;
+     }
+</style>
+"""
+,unsafe_allow_html=True)
+
+#-- Build dashboard
 
 st.button("Refresh Data", on_click=update_all_projects, args=[project_list, team_list, config['jira']['token']], type="primary")
 
@@ -48,7 +72,7 @@ with summary_tab:
 for x in range(0,len(project_list)):
     project = project_list[x]
     project_name = project_name_list[x]
-    df = build_personal_load(project)
+    df = d.build_personal_load(project)
     with columns[col_index]:
         st.write(f"{project_name} tickets")
         st.dataframe(df.style.background_gradient(cmap="Blues", vmin=0, vmax=100), use_container_width=True)
@@ -61,17 +85,17 @@ for x in range(0,len(project_tabs)):
     project = project_list[x]
     project_name = project_name_list[x]
     with project_tabs[x]:
-        df = build_issues(project)
+        df = d.build_issues(project)
         st.write(f"{project_name} Issue board")
         st.markdown(df.style.bar(subset=['life'], color='#d65f5f').hide(axis=0).to_html(), unsafe_allow_html=True)
     
 # Sidebar ----
 st.sidebar.markdown("**Unassigned Issues**")
 for project in project_list:
-    project_value, project_delta = build_assigned_tickets(project)
-    st.sidebar.metric(project, project_value, delta=None, delta_color="normal", help=None, label_visibility="visible")
+    project_value, project_delta = d.build_assigned_project_tickets(project)
+    st.sidebar.metric(project, project_value, delta=project_delta, delta_color="inverse", help=None, label_visibility="visible")
 st.sidebar.markdown("---")
 st.sidebar.markdown("**Total Assigned Issues**")
 for person in team_list: 
-    person_value, person_delta = build_all_assigned_tickets(person, project_list)
-    st.sidebar.metric(person, person_value, delta=None, delta_color="normal", help=None, label_visibility="visible")    
+    person_value, person_delta = d.build_tickets_assigned_for_a_person(project_list, person)
+    st.sidebar.metric(person, person_value, delta=person_delta, delta_color="inverse", help=None, label_visibility="visible")    
--- streamlit/data.py
+++ streamlit/data.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import json
 import configparser
 
@@ -10,103 +12,139 @@ DATA_FILE = "frontend-ensweb"
 ISSUES_FILE = "{}.json"
 SUMMARY_FILE = "{}_summary.json"
 
-def update_data(project, people, token):
+UNASSIGNED = "unassigned"
 
-    personal_issues(
-        "https://www.ebi.ac.uk/panda/jira",
-        token,
-        people,
-        project,
-        project,
-        True
-    )
+"""
+I want to store 
+- last update
+- last values for all metrics
+- 
+"""
 
-def load_data(project):
-    with open(ISSUES_FILE.format(project)) as issue_file:
-        issues = json.load(issue_file)
-    
-    print(f"{project}: {len(issues)} issues found")
-    
-    with open(SUMMARY_FILE.format(project)) as summary_file:
-        summary = json.load(summary_file)
-    
-    print(f"{project}: Summary data found for {len(summary)} people")
-    return issues, summary
+class Data(Enum):
+    All = 1,
+    Issues = 2,
+    Summary = 3,
 
-def build_personal_load(project):
-    
-    issues, summary = load_data(project)
-
-    counts = {}
-    people_counts = {}
-    
-    view = []
-    
-    for person, data in summary.items():
-        person_count = {key:value for key,value in data['issues'].items()}
+class JiraData():
+    def __init__(self, projects): 
+        self.summary = {} 
+        self.issues = {}
+        self.metrics = {}
+        self.projects = {}
         
-        for key,value in person_count.items():
-            if key not in counts:
-                counts[key] = 0
-                
-            if person != 'unassigned':
-                counts[key] += value
+        for project in projects:
+            self.summary[project] = self.load_data(project,Data.Summary)
+            self.issues[project] = self.load_data(project, Data.Issues)
+
+    def update_data(self, project, people, token):
+    
+        personal_issues(
+            "https://www.ebi.ac.uk/panda/jira",
+            token,
+            people,
+            project,
+            project,
+            True
+        )
+    
+    def load_data(self, project, data:Data):
+        
+        if data == Data.All or data == Data.Issues:
+            with open(ISSUES_FILE.format(project)) as issue_file:
+                issues = json.load(issue_file)
+            print(f"{project}: {len(issues)} issues found")
+        
+        if data == Data.All or data == Data.Summary:
+            with open(SUMMARY_FILE.format(project)) as summary_file:
+                summary = json.load(summary_file)
             
-        people_counts[person] = person_count
+            print(f"{project}: Summary data found for {len(summary)} people")
+        
+        if data == Data.All: 
+            return issues, summary
+        if data == Data.Issues: 
+            return issues 
+        if data == Data.Summary: 
+            return summary
     
+    def build_personal_load(self, project):
+        
+        summary = self.summary[project]
     
-    fields = [key for key,_ in counts.items()]
-    
-    for person, data in people_counts.items():
-        flattened = [person]
-        flattened.extend([data.get(key,0) for key in fields])
+        counts = {}
+        people_counts = {}
+        
+        view = []
+        
+        for person, data in summary.items():
+            person_count = {key:value for key,value in data['issues'].items()}
+            
+            for key,value in person_count.items():
+                if key not in counts:
+                    counts[key] = 0
+                    
+                if person != 'unassigned':
+                    counts[key] += value
+                
+            people_counts[person] = person_count
+        
+        
+        fields = [key for key,_ in counts.items()]
+        
+        for person, data in people_counts.items():
+            flattened = [person]
+            flattened.extend([data.get(key,0) for key in fields])
+            view.append(flattened)
+            
+        flattened = ["Everyone"]
+        flattened.extend([value for _, value in counts.items()])
         view.append(flattened)
         
-    flattened = ["Everyone"]
-    flattened.extend([value for _, value in counts.items()])
-    view.append(flattened)
-    
-    columns = ["Who"]
-    columns.extend(fields)
-    
-    df = pd.DataFrame(view, columns = columns)
-
-    return df
-
-def build_assigned_tickets(project, person = "unassigned"):
-    issues, _ = load_data(project)
-    count = 0
-    delta = 0
-    for issue in issues:
-        if issue["person"] == person:
-            count += 1
-    return count, delta
-    
-#----------------
-def build_all_assigned_tickets(person, projects):
-    count = 0
-    delta = 0
-    for project in projects:
-        c,d = build_assigned_tickets(project, person)
-        count += c
-        delta += d
-    return count, delta
+        columns = ["Who"]
+        columns.extend(fields)
         
+        df = pd.DataFrame(view, columns = columns)
     
-
-#---------------
-def build_issues(project):
-    issues, summary = load_data(project)
-    columns = ['issue','summary','person','release','sprint','status','life']
+        return df
     
-    LINK_FORMAT = "<a target=\"new\" href=\"https://www.ebi.ac.uk/panda/jira/browse/{id}\">{id}</a>"
+    #-------------
     
-    view = []
-    for issue in issues:
-        view.append([issue[key] for key in columns])
+    def count_project_tickets_assigned_for_a_person(self, project, person=UNASSIGNED): 
+        issues = self.issues[project]
+        count = 0
+        for issue in issues:
+            if issue["person"] == person:
+                count += 1
+        return count
         
-    for row in view:
-        row[0] = LINK_FORMAT.format(id=row[0])
-
-    df = pd.DataFrame(view, columns=columns)
-    return df
+    def count_tickets_assigned_for_a_person(self, projects, person=UNASSIGNED):
+        count = 0
+        for project in projects:
+            count += self.count_project_tickets_assigned_for_a_person(project, person)
+        return count
+        
+    def build_assigned_project_tickets(self, project, person=UNASSIGNED):
+        current_count = self.count_project_tickets_assigned_for_a_person(project, person)
+        return current_count, 0
+        
+    def build_tickets_assigned_for_a_person(self, projects, person=UNASSIGNED):
+        current_count = self.count_tickets_assigned_for_a_person(projects, person)
+        return current_count, 0
+    
+    #---------------
+    def build_issues(self, project):
+        issues = self.issues[project]
+        columns = ['issue','summary','person','release','sprint','status','life']
+        
+        LINK_FORMAT = "<a target=\"new\" href=\"https://www.ebi.ac.uk/panda/jira/browse/{id}\">{id}</a>"
+        
+        view = []
+        for issue in issues:
+            view.append([issue[key] for key in columns])
+            
+        for row in view:
+            row[0] = LINK_FORMAT.format(id=row[0])
+    
+        df = pd.DataFrame(view, columns=columns)
+        return df
